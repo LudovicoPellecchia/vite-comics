c5d6d530e98ea3207651314cf84fb83d9d22b232
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-08-23 17:33:45 +0200

    aggiunta card

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
 import Banner from "./components/Banner.vue";
-import DcComics from "./db/dc-comics"
+import DcComics from "./db/dc-comics";
+import Card from "./components/Card.vue"
 
 export default {
   components: {
     TheHeader,
     TheFooter,
     Banner,
+    Card,
   },
 
   data() {
@@ -30,6 +32,15 @@ export default {
     <div class="content-wrapper">
       <div class="container">
         <h3 class="content-sct-title">CURRENT SERIES</h3>
+        <div class="row row-cols-6">
+          <div class="col" v-for="card in DcComics">
+            <Card 
+            :img-src="card.thumb"
+            :title="card.series"></Card>
+          </div>
+        </div>
+        <div class="load-content-btn text-center">
+          <span>LOAD MORE</span></div>
       </div>
     </div>
     <Banner></Banner>
@@ -46,19 +57,28 @@ export default {
 }
 
 .content-wrapper {
+  padding-bottom: 30px;
   background-color: #1C1C1C;
 
   h3 {
     color: white;
   }
 
-  .content-sct-title{
-    background-color:#0282F9 ;
+  .content-sct-title {
+    background-color: #0282F9;
     padding: 8px 20px;
     margin-top: -30px;
     margin-bottom: 30px;
     display: inline-block;
   }
 
+  .load-content-btn span{
+    display: inline-block;
+    background-color: #0282F9;
+    padding: 10px 50px;
+    font-weight: 600;
+    color: white;
+
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    props: {
+        imgSrc: String,
+        title: String,
+    }
+
+
+}
+</script>
+
+<template>
+    <div class="card border-0">
+        <img :src="imgSrc" class="card-img-top rounded-0" alt="...">
+        <h5>{{ title.toUpperCase() }}</h5>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.card{
+    background-color:  #1C1C1C;
+}
+
+img {
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    object-position: top;
+}
+
+h5{
+    font-size: 1.2rem;
+    font-weight: 300;
+    padding-top: 15px;
+    color: white;
+}
+</style>
